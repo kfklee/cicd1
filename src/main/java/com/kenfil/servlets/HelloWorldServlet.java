a4d863554fdf57cf366393a093e2cf8485b2927f
kfklee <kelvin.lkf.lee@gmail.com>
2024-11-22 16:33:20 +0800

    Change the text

--- src/main/java/com/kenfil/servlets/HelloWorldServlet.java
+++ src/main/java/com/kenfil/servlets/HelloWorldServlet.java
@@ -13,7 +13,14 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
+
+            // Get the "name" parameter from the request. Default to "world" if not provided.
+    String name = request.getParameter("name");
+    if (name == null || name.isEmpty()) {
+      name = "world";
+    }
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    
+    response.getWriter().println("<h1>Hello, " + name + "!</h1>");
   }
 }
